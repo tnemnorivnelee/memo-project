027d4d88bc69261aa7a6b72e27411c1ab7d71d8f
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-02-19 02:49:06 +0900

    feat: 메모 수정 기능 및 메모 선택 기능 구현현

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,8 +1,18 @@
-function MemoList({ memos }) {
+import MemoItem from './MemoItem';
+
+function MemoList({ memos, setSelectedMemoIndex, selectedMemoIndex }) {
   return (
     <div>
       {memos.map((memo, index) => (
-        <div key={index}>{memo.title}</div>
+        <MemoItem
+          key={index}
+          onClick={() => {
+            setSelectedMemoIndex(index);
+          }}
+          isSelected={index === selectedMemoIndex}
+        >
+          {memo.title}
+        </MemoItem>
       ))}
     </div>
   );
